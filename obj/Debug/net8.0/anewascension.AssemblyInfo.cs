4b80e0560543c4944eec0764d04cb5732b9bbd03
WardenMeow <thepinklion17@gmail.com>
2026-08-12 02:34:30 -0400

    A new dawn

--- obj/Debug/net8.0/anewascension.AssemblyInfo.cs
+++ obj/Debug/net8.0/anewascension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3518571c1eb42666cad34064bc8a339e594632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab275bf096b05801e231b62c1f3260c5e85503b")]
 [assembly: System.Reflection.AssemblyProductAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
