aa08c72ddf8b4d07fbd68ce424a2cdac39671315
WardenMeow <thepinklion17@gmail.com>
2026-08-12 00:02:57 -0400

    FUCKTHESENATE

--- obj/Debug/net8.0/anewascension.AssemblyInfo.cs
+++ obj/Debug/net8.0/anewascension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0f75d922fbd72597c7af222cde882b4386e947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3cebd77d41a501954d6c005e393d061352ea807")]
 [assembly: System.Reflection.AssemblyProductAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
