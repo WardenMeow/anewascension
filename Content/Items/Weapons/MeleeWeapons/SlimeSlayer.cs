0f368efb6effddfd5eb923c95e34e1aa4abbb1ed
WardenMeow <thepinklion17@gmail.com>
2026-08-11 23:16:42 -0400

    brackets AGAIN

--- Content/Items/Weapons/MeleeWeapons/SlimeSlayer.cs
+++ Content/Items/Weapons/MeleeWeapons/SlimeSlayer.cs
@@ -8,10 +8,10 @@ namespace anewascension.Content.Items.Weapons.MeleeWeapons
 {
     public class SlimeSlayer : ModItem
     {
-        public override void SetDefaults() {
+        public override void SetDefaults()
+        {
             Item.width = 26;
             Item.height = 42;
-        
 
             Item.useStyle = ItemUseStyleID.Swing;
             Item.useTime = 20;
@@ -29,40 +29,49 @@ namespace anewascension.Content.Items.Weapons.MeleeWeapons
 
             Item.shoot = ProjectileID.StarWrath;
             Item.shootSpeed = 8f;
-        }    
-	
+        }
 
-    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-			Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
-			float ceilingLimit = target.Y;
-			if (ceilingLimit > player.Center.Y - 200f) {
-				ceilingLimit = player.Center.Y - 200f;
-			}
-			// Loop these functions 3 times.
-			for (int i = 0; i < 3; i++) {
-				position = player.Center - new Vector2(Main.rand.NextFloat(401) * player.direction, 600f);
-				position.Y -= 100 * i;
-				Vector2 heading = target - position;
-            
-				if (heading.Y < 0f) {
-					heading.Y *= -1f;
-				}
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
+            float ceilingLimit = target.Y;
 
-				if (heading.Y < 20f) {
-					heading.Y = 20f;
-				}
+            if (ceilingLimit > player.Center.Y - 200f)
+            {
+                ceilingLimit = player.Center.Y - 200f;
+            }
 
-				heading.Normalize();
-				heading *= velocity.Length();
-				heading.Y += Main.rand.Next(-40, 41) * 0.02f;
-				Projectile.NewProjectile(source, position, heading, type, damage * 2, knockback, player.whoAmI, 0f, ceilingLimit);
-			}
+            for (int i = 0; i < 3; i++)
+            {
+                position = player.Center - new Vector2(Main.rand.NextFloat(401) * player.direction, 600f);
+                position.Y -= 100 * i;
 
-            public override Vector2? HoldoutOffset() {
-    return new Vector2(10, -2); // Adjust X and Y values
-}
+                Vector2 heading = target - position;
 
-			return false;
+                if (heading.Y < 0f)
+                {
+                    heading.Y *= -1f;
+                }
+
+                if (heading.Y < 20f)
+                {
+                    heading.Y = 20f;
+                }
+
+                heading.Normalize();
+                heading *= velocity.Length();
+                heading.Y += Main.rand.Next(-40, 41) * 0.02f;
+
+                Projectile.NewProjectile(source, position, heading, type, damage * 2, knockback, player.whoAmI, 0f, ceilingLimit);
+            }
+
+            return false;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(10, -2);
+        }
     }
 }
-}
+
