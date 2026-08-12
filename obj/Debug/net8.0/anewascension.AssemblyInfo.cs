51df199bf845b10e94f0932313a48469ad163bd2
WardenMeow <thepinklion17@gmail.com>
2026-08-11 23:49:26 -0400

    FINALFINALFINALFINAL FOR REAL THIS TIME

--- obj/Debug/net8.0/anewascension.AssemblyInfo.cs
+++ obj/Debug/net8.0/anewascension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c81e350d52d0c56b7f056347db928c0ea0db289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c1254bcf46d5a1ecacba4e8ced5e31e0e76083")]
 [assembly: System.Reflection.AssemblyProductAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
