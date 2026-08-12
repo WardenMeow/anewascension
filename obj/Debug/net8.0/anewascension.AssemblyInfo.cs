07efca3fe1335dceb3234afc36f47ceadd1dbfc5
WardenMeow <thepinklion17@gmail.com>
2026-08-11 22:51:20 -0400

    added sprite SlimeSlayer

--- obj/Debug/net8.0/anewascension.AssemblyInfo.cs
+++ obj/Debug/net8.0/anewascension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3275d5b76a388e3669dd21b6fd1642238ce0991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea532f178930329025f14710e7a9a87e6a7bc30")]
 [assembly: System.Reflection.AssemblyProductAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
