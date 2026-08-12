61fe132c2eadcfc424887bd848f33ed624a4122a
WardenMeow <thepinklion17@gmail.com>
2026-08-12 02:36:35 -0400

    sometimes

--- anewascension.cs
+++ anewascension.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 namespace anewascension
 {
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class anewascension : Mod
+	public class Anewascension : Mod
 	{
 
 	}
