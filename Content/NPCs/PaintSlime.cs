adccc4a75ae4c3d02aca40bb07abae4b9186b398
WardenMeow <thepinklion17@gmail.com>
2026-08-11 21:48:54 -0400

    updated drop pool bug

--- Content/NPCs/PaintSlime.cs
+++ Content/NPCs/PaintSlime.cs
@@ -33,9 +33,9 @@ namespace anewascension.Content.NPCs
 
     public override void ModifyNPCLoot(NPCLoot npcLoot)
     {
-        npcLoot.Add(ItemDropRule.Common(ItemID.1073, 3, 5, 10));
-        npcLoot.Add(ItemDropRule.Common(ItemID.1081, 1, 5, 10));
-        npcLoot.Add(ItemDropRule.Common(ItemID.1967, 2, 5, 10));
+        npcLoot.Add(ItemDropRule.Common(ItemID.RedPaint, 3, 5, 10));
+        npcLoot.Add(ItemDropRule.Common(ItemID.BluePaint, 1, 5, 10));
+        npcLoot.Add(ItemDropRule.Common(ItemID.ShadowPaint, 2, 5, 10));
     }
 
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
