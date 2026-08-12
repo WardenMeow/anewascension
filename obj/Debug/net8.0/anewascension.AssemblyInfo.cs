990980f7d5fac431322da28d6b01e80567744062
WardenMeow <thepinklion17@gmail.com>
2026-08-11 23:38:35 -0400

    maybe?

--- obj/Debug/net8.0/anewascension.AssemblyInfo.cs
+++ obj/Debug/net8.0/anewascension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f368efb6effddfd5eb923c95e34e1aa4abbb1ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d21f6b2e70c273ec7e807dc1651bad043df21c")]
 [assembly: System.Reflection.AssemblyProductAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anewascension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
